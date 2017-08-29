c2d73b5c44df403fcd0a4fe0cd9aafca84878e36
oscarsalu <oscarsalu@gmail.com>
2017-08-29 18:17:07 +0300

    complete

---
+++ routes/contact.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require(nodemailer);
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('contact', { title: 'Contact' });
+});
+
+router.post('/send', function(req, res, next) {
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'email',
+            pass: 'somthing'
+        }
+    });
+    var mainOptions = {
+        from: req.body.email,
+        to: 'email',
+        subject: 'Website Submission for ' + req.body.name,
+        text: req.body.message,
+        html: '<p>You got a new submission with the following</p><ul><li>Name:' + req.body.name + '</li><li>Email:' + req.body.email + '</li><li>Message:' + req.body.message + '</li></ul>'
+    };
+
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+            res.redirect('/');
+        } else {
+            console.log('Message Sent' + info.message);
+            res.redirect('/');
+        }
+    });
+});
+
+module.exports = router;
